26f84a351a3b7f38cac767ee7d12e4ab538ea0c0
Elukoye <lukoyedith@gmail.com>
2025-05-25 06:49:58 +0300

    refactor code for calculator

---
+++ calculator_refactor.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+operator = input("Choose operator + , - , * , / :___")
+result = None
+explanation =""
+
+def handle_addition(num1,num2):
+    return result,explanation
+    
